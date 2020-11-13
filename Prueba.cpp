6224e7a09c2318244d521bc8f7676713be266094
Diegorem99 <74273897+Diegorem99@users.noreply.github.com>
2020-11-13 10:00:59 -0600

    Update Prueba.cpp

--- Prueba.cpp
+++ Prueba.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 int main()
 {
-    cout << "Hola mundo!" << endl;
-    cout << "Éste es un tutorial de GitHub" << endl;
-    cout << "Éste es el último cambio que realizaremos" << endl;
-    
+    cout << "Hola mundo!" << endl;    
     return 0;
 }
